5806eddb7ff84f1192ae31b543abdd4910306b8d
Manuel Jesus Tello May <manuel.tello@tecdesoftware.edu.mx>
2026-06-02 11:30:26 -0600

    feat: implement ProductoRepository and custom query methods - Add custom query methods in ProductoCrudRepository (sorted category filter and low stock check).- Create ProductoRepository to wrap CRUD operations (getAll, getByCategoria, getEscasos, getProductoById, save). - Implement ProductoRepository class with getAll() method to fetch products.

--- src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/persistence/ProductoRepository.java
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/persistence/ProductoRepository.java
@@ -4,6 +4,7 @@ import mx.edu.tecdesoftware.market_backend_2026_3_b.persistence.crud.ProductoCru
 import mx.edu.tecdesoftware.market_backend_2026_3_b.persistence.entities.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productocrudrepository;
@@ -12,5 +13,30 @@ public class ProductoRepository {
     public List<Producto> getAll(){
         //Se castea Iterable a la lista
         return(List<Producto>) productocrudrepository.findAll();
+    }
+
+    //Obtener productos por categoria
+    public List<Producto> getByCategoria(int idCategoria){
+        return productocrudrepository.findByCantidadOrderByNombreAsc(idCategoria);
+    }
+
+   //Obtener productos escasos
+   public Optional<List<Producto>> getEscasos(int cantidad){
+        return productocrudrepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+   }
+
+   //Obtener producto dado el ID
+    public Optional<Producto> getProductoById(int idProducto){
+        return productocrudrepository.findById(idProducto);
+    }
+
+    //Guardar un producto
+    public Producto addProducto(Producto producto){
+        return productocrudrepository.save(producto);
+    }
+
+   // Eliminar un producto por ID
+    public void deleteProductoById(int idProducto){
+        productocrudrepository.deleteById(idProducto);
     }
 }
--- src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,24 @@ package mx.edu.tecdesoftware.market_backend_2026_3_b.persistence.crud;
 import mx.edu.tecdesoftware.market_backend_2026_3_b.persistence.entities.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
+
+//Obtener la lista de productos filtrados por
+// id categoria y ordenados ascendentemente por nombre
+
+/*
+   SELECT *
+   FROM Categorias
+   WHERE id_categoria = ?
+   ORDER BY Nombre ASC
+ */
+
+   List<Producto> findByCantidadOrderByNombreAsc (int idCategoria);
+
+   //Obtener los productos
+   Optional<List<Producto>> findByCantidadStockLessThanAndEstado (int cantidad, boolean estado);
+
 }
